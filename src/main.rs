39d5f0bfd1b8743d0c2577e080cbefa6afca69be
Daniel Duque <daniel.duque@cern.ch>
2024-09-22 22:45:26 +0200

    feat(cli): Add clap arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(version)]
+/// Create an elog for a single run
+struct Args {
+    /// ALPHA-g run number
+    run_number: u32,
+    /// Path to a configuration file (overrides the default configuration)
+    config_file: Option<PathBuf>,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    Ok(())
 }
